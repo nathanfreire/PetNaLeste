b4ff4c1bbb9f2bb7b6031b342d9135de0d6d380b
Nathan Freire <nathan.freire1403@gmail.com>
2025-06-10 14:28:59 -0300

    Atualização para as normas SOLID

--- Back-and/src/database/Config.ts
+++ Back-and/src/database/Config.ts
@@ -1,10 +1,10 @@
-/* import mysql from "mysql2"
+import mysql from "mysql2"
 
 export const conexao = mysql.createConnection({
-    host:"10.26.45.1",
+    host:"10.26.45.28",
     port:3307,
     user:"root",
     password:"123456",
     database:"PetNaLeste"
 }); 
-*/
+
